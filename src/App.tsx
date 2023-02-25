042164db35c4690f400f6d71208a4f36fb6941ba
vilask <vilask@iconnectsolutions.com>
2023-02-25 11:21:09 +0530

    Register component added, router added

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import RegisterClient from './Register';
+
+const ListClients = (props:any) => { 
+  // todo use client list table comp
+  return (
+    <h1>ListUsers</h1>
+  )
+ }
+
+export  function Navigation() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/listClients">List clients</Link>
+            </li>
+            <li>
+              <Link to="/register">register</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/register">
+            <RegisterClient />
+          </Route>
+          <Route path="/listClients">
+            <ListClients />
+          </Route>
+          <Route path="/" exact = {true} >
+            <RegisterClient />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 
 function App() {
   return (
-    <div className="bg-dark">
-      <header className="mx-auto w-50">
-        <img src={logo} className=" img-thumbnail" style = {{width:'120px'}} alt="logo" />
-        <p className='text-primary' >
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+    <div >
+      <Navigation />
     </div>
   );
 }
