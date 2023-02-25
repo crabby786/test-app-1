3912dd47e1a1fde391e31a6a5a67a7799237f92c
vilask <vilask@iconnectsolutions.com>
2023-02-25 09:30:59 +0530

    bootstrap added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="bg-dark">
+      <header className="mx-auto w-50">
+        <img src={logo} className=" img-thumbnail" style = {{width:'120px'}} alt="logo" />
+        <p className='text-primary' >
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
